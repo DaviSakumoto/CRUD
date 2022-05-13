1bbfe676107191e923e44acd127ed8382c555a8a
davi <dsakumot@gmail.com>
2022-05-13 18:37:50 -0300

    update

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 var dados = []
 
 
+function populaTabela(){
+    if(Array.isArray(dados)) {
+
+      $("#tblDados tbody").html("")
+
+      dados.forEach(function(item){
+          $("#tblDados tbody").append(`<tr>
+          <td></td>
+          </tr>`);
+      });
+    }
+}
 
 $(function (){
  // EXECUTA AO CARREGAR DA TELA
 
-   dados =  localStorage.getItem("__dados__");
+   dados =  JSON.parse(localStorage.getItem("__dados__"));
 
    if(dados){
-
+    populaTabela()
    }
 
-});
+})
